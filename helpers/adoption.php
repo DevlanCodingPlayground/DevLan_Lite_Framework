31d00fdef4425d9e98215fd1654fec62c851eafe
ShirleneNait <shirlruwa@gmail.com>
2022-11-07 21:20:40 +0300

    all unit test pass on card payments

--- helpers/adoption.php
+++ helpers/adoption.php
@@ -1,6 +1,6 @@
 <?php
 // update adoption 
-if(isset($_POST['update_pet_adoption'])){
+if (isset($_POST['update_pet_adoption'])) {
     $pet_adoption_id = mysqli_real_escape_string($mysqli, $_POST['pet_adoption_id']);
     $pet_adoption_date = mysqli_real_escape_string($mysqli, $_POST['pet_adoption_date']);
 
@@ -10,10 +10,9 @@ if(isset($_POST['update_pet_adoption'])){
     } else {
         $err = "Sorry.failed to update adoption record.";
     }
-
 }
 //return pet
-if(isset($_POST['return_pet'])){
+if (isset($_POST['return_pet'])) {
     $pet_adoption_id = mysqli_real_escape_string($mysqli, $_POST['pet_adoption_id']);
     $pet_id = mysqli_real_escape_string($mysqli, $_POST['pet_id']);
 
@@ -21,12 +20,11 @@ if(isset($_POST['return_pet'])){
     $update_pet_sql = "UPDATE pet SET pet_adoption_status= 'Available' WHERE pet_id = '{$pet_id}'";
 
 
-    if (mysqli_query($mysqli, $update_sql) && mysqli_query($mysqli, $update_pet_sql) ) {
+    if (mysqli_query($mysqli, $update_sql) && mysqli_query($mysqli, $update_pet_sql)) {
         $success = "Pet returned";
     } else {
         $err = "Sorry.failed to update adoption record.";
     }
-
 }
 /* Add Payment */
 if (isset($_POST['Add_Payment'])) {
@@ -52,14 +50,66 @@ if (isset($_POST['Add_Payment'])) {
         }
     } else if ($payment_means == 'Credit / Debit Card') {
         /* Handle Credit/Debit Card - To Avoid Messy Codebases Just Include The File Here */
-        include('../api/flutterwave/process_payment.php');
+        // include('../api/flutterwave/process_payment.php');
+
+        /* Prepare Fluterwave RAVE API Request */
+        $request = [
+            'tx_ref' => $payment_ref,
+            'amount' => $payment_amount,
+            'currency' => 'KES',
+            'payment_options' => 'card',
+            /* Update This URL To Match Your Needs */
+            'redirect_url' => 'http://127.0.0.1/iPet/views/payment_response.php?adoption=' . $payment_pet_adoption_id,
+            'customer' => [
+                'email' => $pet_adopter_email,
+                'name' => $pet_adopter_name,
+            ],
+            'meta' => [
+                'price' => $payment_amount
+            ],
+            'customizations' => [
+                'title' => 'Pet Adoption Payment',
+                'description' => $pet_adopter_name . 'Pet Adoption Payment'
+            ]
+        ];
+
+        /* Call Flutterwave Endpoint */
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.flutterwave.com/v3/payments',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($request),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer FLWSECK_TEST-a90855faf858298f0b14bfb4621e53fe-X', /* To Do : Never hard code this bearer */
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $res = json_decode($response);
+        if ($res->status == 'success') {
+            $link = $res->data->link;
+            header('Location: ' . $link);
+        } else {
+            $err =  'We can not process your payment';
+        }
     } else {
         /* Handle Mobile Payments - To Avoid Messy Codebases Just Include The File Here */
         $err = "Payment method is not supported";
     }
 }
 //delete
-if(isset($_POST['delete_adoption'])){
+if (isset($_POST['delete_adoption'])) {
     $pet_adoption_id = mysqli_real_escape_string($mysqli, $_POST['pet_adoption_id']);
     $pet_id = mysqli_real_escape_string($mysqli, $_POST['pet_id']);
 
@@ -67,10 +117,9 @@ if(isset($_POST['delete_adoption'])){
     $update_pet_sql = "UPDATE pet SET pet_adoption_status= 'Available' WHERE pet_id = '{$pet_id}'";
 
 
-    if (mysqli_query($mysqli, $delete_sql) && mysqli_query($mysqli, $update_pet_sql) ) {
+    if (mysqli_query($mysqli, $delete_sql) && mysqli_query($mysqli, $update_pet_sql)) {
         $success = "Pet adoption record deleted";
     } else {
         $err = "Sorry.failed to update adoption record.";
     }
-
 }
